31be31075fdfc786f428cce67644aeda107041b3
Bruno Macedo <brunom@ciandt.com>
2018-07-23 00:51:44 -0300

    finish login and registration

--- connectedin/settings.py
+++ connectedin/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'connectedin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL="/login"
+LOGOUT_URL="/logout"
+LOGIN_REDIRECT_URL="/"
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from usuarios.views import RegistrarUsuarioView
+from django.contrib.auth import views
 
 urlpatterns = [
-    path('registrar', RegistrarUsuarioView.as_view(), name='registrar')
+    path('registrar', RegistrarUsuarioView.as_view(), name='registrar'),
+    path('login', views.login, {'template_name':'login.html'}, name='login'),
+    path('logout', views.logout_then_login, {'login_url':'/login'}, name='logout')
+ 
 ]
